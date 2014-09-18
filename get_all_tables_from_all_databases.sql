60488847974246fd741db9c7aee66f6c4224d0a5
richiverse <richard.fernandez.nyc@gmail.com>
2014-09-18 14:30:04 -0400

    Create get_all_tables_from_all_databases.sql

---
+++ get_all_tables_from_all_databases.sql
@@ -0,0 +1,6 @@
+SET NOCOUNT ON
+DECLARE @AllTables table (CompleteTableName nvarchar(4000))
+INSERT INTO @AllTables (CompleteTableName)
+    EXEC sp_msforeachdb 'select @@SERVERNAME+''.''+''?''+''.''+s.name+''.''+t.name from [?].sys.tables t inner join sys.schemas s on t.schema_id=s.schema_id'
+SET NOCOUNT OFF
+SELECT * FROM @AllTables ORDER BY 1
