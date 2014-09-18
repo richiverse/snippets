df200b7dd8db26f9aac7de7867463f97b310816b
richiverse <richard.fernandez.nyc@gmail.com>
2014-09-18 14:35:52 -0400

    Update get_all_tables_from_all_databases.sql
    sql server

--- get_all_tables_from_all_databases.sql
+++ get_all_tables_from_all_databases.sql
@@ -4,3 +4,4 @@ INSERT INTO @AllTables (CompleteTableName)
     EXEC sp_msforeachdb 'select @@SERVERNAME+''.''+''?''+''.''+s.name+''.''+t.name from [?].sys.tables t inner join sys.schemas s on t.schema_id=s.schema_id'
 SET NOCOUNT OFF
 SELECT * FROM @AllTables ORDER BY 1
+ 
